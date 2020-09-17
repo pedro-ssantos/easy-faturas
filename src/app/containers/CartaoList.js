4d2b4f3446f64faf4b3fef38e0198b19fe8ffaae
Pedro Santos <peusantos@gmail.com>
2020-09-17 19:08:12 -0300

    Adds Cartao list page.

---
+++ src/app/containers/CartaoList.js
@@ -0,0 +1,87 @@
+import * as React from 'react';
+import { ScrollView, StyleSheet, View } from 'react-native';
+import { Appbar, BottomNavigation, Card, List, FAB, Text, Title, Paragraph, } from 'react-native-paper';
+
+const HomeRoute = () => <Text>Music</Text>;
+
+const FaturaRoute = () => <Text>Albums</Text>;
+
+const CartaoRoute = () => <Text>Recents</Text>;
+
+export default function Dashboard() {
+
+  const [index, setIndex] = React.useState(2);
+  const [routes] = React.useState([
+    { key: 'home', title: 'Home', icon: 'home' },
+    { key: 'fatura', title: 'Fatura', icon: 'list-alt' },
+    { key: 'cartao', title: 'Cartão', icon: 'credit-card-outline' },
+  ]);
+
+  const renderScene = BottomNavigation.SceneMap({
+    home: HomeRoute,
+    fatura: FaturaRoute,
+    cartao: CartaoRoute,
+  });
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    fab: {
+      position: 'absolute',
+      margin: 16,
+      bottom: 0,
+    },
+    ContainerFAB: {
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    image: {
+      height: 40,
+      width: 40,
+      margin: 8,
+    },
+  })
+
+  return (
+    <>
+      <View>
+        <Appbar>
+          <Appbar.Content title='Pedro' />
+          <Appbar.Action icon="exit-to-app" onPress={() => { }} />
+        </Appbar>
+      </View>
+      <ScrollView style={[styles.container]}>
+        <List.Section>
+          <List.Item
+            left={props => <List.Icon {...props} icon="credit-card-outline" />}
+            title="Cartão 1"
+          />
+          <List.Item
+            left={props => <List.Icon {...props} icon="credit-card-outline" />}
+            title="Cartão 2"
+          />
+          <List.Item
+            title="Cartão 3"
+            left={props => <List.Icon {...props} icon="credit-card-outline" />}
+          />
+        </List.Section>
+      </ScrollView>
+      <View style={styles.ContainerFAB}>
+        <FAB
+          style={styles.fab}
+          label="Adicionar Cartão"
+          onPress={() => { }}
+        />
+      </View>
+      <View>
+        <BottomNavigation
+          navigationState={{ index, routes }}
+          onIndexChange={setIndex}
+          renderScene={renderScene}
+          sceneAnimationEnabled={false}
+        />
+      </View>
+    </>
+  )
+}
