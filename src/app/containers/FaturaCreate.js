6f933f46359893e31f4fbd298f3fe956d426f3d1
Pedro Santos <peusantos@gmail.com>
2020-09-17 18:25:13 -0300

    Adds Fatura Create page.

---
+++ src/app/containers/FaturaCreate.js
@@ -0,0 +1,63 @@
+import * as React from 'react'
+import { Image, View, StyleSheet, KeyboardAvoidingView, Text } from 'react-native'
+import { Appbar, Button, TextInput } from 'react-native-paper'
+
+export default function Register() {
+
+  return (
+    <>
+      <Appbar.Header>
+        <Appbar.BackAction onPress={() => navigation.goBack()} />
+        <Appbar.Content title="Nova Fatura" />
+      </Appbar.Header>
+      <KeyboardAvoidingView style={styles.background}>
+        <View style={styles.container}>
+          <TextInput
+            mode="outlined"
+            style={styles.inputContainerStyle}
+            label="Cartão"
+            placeholder="Operadora"
+            onChangeText={() => { }}
+          />
+          <TextInput
+            mode="outlined"
+            style={styles.inputContainerStyle}
+            label="Vencimento"
+            placeholder="dd/mm/aaaa"
+            onChangeText={() => { }}
+          />
+          <TextInput
+            mode="outlined"
+            style={styles.inputContainerStyle}
+            label="Valor"
+            placeholder="R$ 000,00"
+            onChangeText={() => { }}
+          />
+          <Button mode="contained" onPress={() => { }} style={styles.button}>
+            SALVAR
+          </Button>
+        </View>
+      </KeyboardAvoidingView>
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  container: {
+    flex: 1,
+
+    width: '90%',
+  },
+  inputContainerStyle: {
+    margin: 8,
+  },
+  button: {
+    margin: 8,
+  },
+})
